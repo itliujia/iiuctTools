c13a7acab1e47e4538d81ff7f1f8b251209c556c
itliujia <liujia0716@vip.qq.com>
2022-11-10 21:02:31 +0800

    in

--- src/directives/copy.ts
+++ src/directives/copy.ts
@@ -12,20 +12,20 @@ export default {
 			// 		// This can happen if the user denies clipboard permissions
 			// 		ElMessage.error('无法复制此文本：', err)
 			// 	})
-
-				const textarea = document.createElement('textarea')
-				textarea.readOnly = false
-				textarea.style.position = 'absolute'
-				textarea.style.left = '-9999px'
-				textarea.value = el.$value
-				document.body.appendChild(textarea)
-				textarea.select()
-				textarea.setSelectionRange(0, textarea.value.length)
-				const result = document.execCommand('Copy')
-				if (result) {
-					ElMessage.success("复制成功")
-				}
-				document.body.removeChild(textarea)
+			//ce's
+			const textarea = document.createElement('textarea')
+			textarea.readOnly = false
+			textarea.style.position = 'absolute'
+			textarea.style.left = '-9999px'
+			textarea.value = el.$value
+			document.body.appendChild(textarea)
+			textarea.select()
+			textarea.setSelectionRange(0, textarea.value.length)
+			const result = document.execCommand('Copy')
+			if (result) {
+				ElMessage.success("复制成功")
+			}
+			document.body.removeChild(textarea)
 
 
 
